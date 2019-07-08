d8b1116f21120452bfa845fc4d5bd3e5568c4431
Mrc.Y <47876053+MarcoYoung@users.noreply.github.com>
2019-07-08 17:42:08 +0800

    Update DList.hpp

--- class/List/DList.hpp
+++ class/List/DList.hpp
@@ -1,7 +1,7 @@
 //
 //  DList.hpp
 //  MyList
-//
+//  实现双向循环链表类，实现了简单的增(insert)、查(find)、删(delete)、遍历(traverse)、冒泡排序(popsort)（交换数据和交换结点两个版本）。
 //  Created by Finy_Caniove on 2019/7/6.
 //  Copyright © 2019 Finy_Caniove. All rights reserved.
 //
