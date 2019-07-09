0be5d884728b518f7d08d654cdb300970522e84e
Mrc.Y <47876053+MarcoYoung@users.noreply.github.com>
2019-07-09 11:05:01 +0800

    Update mylist.h

--- List/C/mylist.h
+++ List/C/mylist.h
@@ -1,3 +1,10 @@
+//
+//  mylist.h
+//  MyList
+//  实现单向链表类，实现了简单的增(insert)、查(find)、删(delete)、遍历(traverse)、冒泡排序(popsort)、逆转(reverse)、销毁(destroy)。
+//  Created by Finy_Caniove on 2019/7/9.
+//  Copyright © 2019 Finy_Caniove. All rights reserved.
+//
 #ifndef MYLIST_H
 #define MYLIST_H
 
